edf45a855d202e6b442244c26e9fed971ffc9b20
Lyal Avery <lyal.avery@gmail.com>
2017-10-31 12:24:22 -0500

    Removed bad composer file

--- src/Laravel/Http/Controllers/CheckrController.php
+++ src/Laravel/Http/Controllers/CheckrController.php
@@ -1,7 +1,12 @@
 <?php
 namespace Lyal\Checkr\Laravel\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
-class CheckrController
+class CheckrController extends Controller
 {
+    public function handleWebHook(Request $request) {
+
+    }
 
 }
